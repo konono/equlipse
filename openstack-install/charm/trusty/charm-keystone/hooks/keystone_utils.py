5427ed1cbe9cf4174918ff59db8157edc80c4256
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-14 18:31:21 +0000

    Adding tarfile member sanitization to extractall()

--- openstack-install/charm/trusty/charm-keystone/hooks/keystone_utils.py
+++ openstack-install/charm/trusty/charm-keystone/hooks/keystone_utils.py
@@ -1473,7 +1473,26 @@ def update_certs_if_available(f):
             log("Updating certs from '%s'" % (path), level=DEBUG)
             with tarfile.open(path) as fd:
                 files = ["/%s" % m.name for m in fd.getmembers()]
-                fd.extractall(path='/')
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(fd, path="/")
 
             for syncfile in files:
                 ensure_permissions(syncfile, user='keystone', group='keystone',
